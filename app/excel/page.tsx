7bdcb802db9597f6b422ea6cd4b1037336f43640
brunitaa <167141335+brunitaa@users.noreply.github.com>
2025-04-03 16:35:38 -0400

    Subir CSV

---
+++ app/excel/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import UploadCSV from "@/components/Excel/UploadCSV";
+
+
+export default function UploadCSVPage() {
+  
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      {/* Aquí debe ir la estructura de tu página */}
+
+      <UploadCSV />
+    </main>
+  );
+}
