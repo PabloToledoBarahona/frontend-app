df9877ce10a2437d0e75c2773bbdee84ac1a8702
Pablo Toledo <pali.toledo.b@gmail.com>
2025-04-03 00:21:51 -0400

    Espaciado agregado a las cards

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 export const Card = ({ children, className = "", ...props }: CardProps) => {
   return (
     <div
-      className={`rounded-2xl bg-white text-gray-900 p-8 shadow-xl ${className}`}
+      className={`w-full max-w-xl mx-auto rounded-2xl bg-white text-gray-900 p-8 shadow-xl ${className}`}
       {...props}
     >
       {children}
